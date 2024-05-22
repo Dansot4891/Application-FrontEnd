23747d8b93b1bbb19da5cf9b8f648bdf972e601c
Dansot4891 <139696331+Dansot4891@users.noreply.github.com>
2024-05-22 23:48:36 +0900

    [chore] websocket 실패 추후 수정 예정

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,81 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:gproject/common/dio/dio.dart';
 import 'package:gproject/common/view/splash_screen.dart';
-import 'package:web_socket_channel/io.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
 
 late Size ratio;
 
+void main() {
+  runApp(
+    ProviderScope(
+      child: const MyApp(),
+    ),
+  );
+}
+
 class MyApp extends StatefulWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  final channel = IOWebSocketChannel.connect('ws://http://ceprj.gachon.ac.kr:60006/ws');
-
   @override
   Widget build(BuildContext context) {
     ratio = Size(MediaQuery.sizeOf(context).width / 412, MediaQuery.sizeOf(context).height / 892);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: "Pretendard"),
-      home: StreamBuilder(
-        stream: channel.stream,
-        builder: (context, snapshot){
-          return SplashScreen();
-        },
-      ),
+      home: SplashScreen(),
     );
   }
-
-  @override
-  void dispose() {
-    super.dispose();
-    channel.sink.close();
-  }
 }
 
-void main() {
-  runApp(
-    ProviderScope(
-      child: MyApp(),
-    ),
-  );
-}
+
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:gproject/common/view/splash_screen.dart';
+// import 'package:web_socket_channel/web_socket_channel.dart';
+
+// late Size ratio;
+
+// class MyApp extends StatefulWidget {
+//   MyApp({super.key});
+
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   final channel = WebSocketChannel.connect(Uri.parse('ws://ceprj.gachon.ac.kr:60006/ws'));
+
+//   @override
+//   Widget build(BuildContext context) {
+//     ratio = Size(MediaQuery.sizeOf(context).width / 412, MediaQuery.sizeOf(context).height / 892);
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(fontFamily: "Pretendard"),
+//       home: StreamBuilder(
+//         stream: channel.stream,
+//         builder: (context, snapshot){
+//           return SplashScreen();
+//         },
+//       ),
+//     );
+//   }
+
+//   @override
+//   void dispose() {
+//     super.dispose();
+//     channel.sink.close();
+//   }
+// }
+
+// void main() {
+//   runApp(
+//     ProviderScope(
+//       child: MyApp(),
+//     ),
+//   );
+// }
